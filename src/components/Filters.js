352afe37f8cdf2ce55457d452175089b03e70cb6
Bitupan Patowary <86779337+bitucodes@users.noreply.github.com>
2023-09-27 23:18:03 +0530

    first commit

---
+++ src/components/Filters.js
@@ -0,0 +1,73 @@
+import { Button, Form } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+
+const Filters = () => {
+  const {
+    productDispatch,
+    productState: { byStock, byFastDelivery, sort, byRating },
+  } = CartState();
+
+  // make state for rating
+
+  return (
+    <div className="filters">
+      <span className="title">Filter</span>
+      <span>
+        <Form.Check
+          inline
+          label="Include Out of Stock"
+          name="group1"
+          type="checkbox"
+          id={`inline-3`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_STOCK",
+            })
+          }
+          checked={byStock}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Fast Delivery Only"
+          name="group1"
+          type="checkbox"
+          id={`inline-4`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_DELIVERY",
+            })
+          }
+          checked={byFastDelivery}
+        />
+      </span>
+      <span>
+        <label style={{ paddingRight: 10 }}>Rating: </label>
+        <Rating
+          rating={byRating}
+          onClick={(i) =>
+            productDispatch({
+              type: "FILTER_BY_RATING",
+              payload: i + 1,
+            })
+          }
+          style={{ cursor: "pointer" }}
+        />
+      </span>
+      <Button
+        variant="light"
+        onClick={() =>
+          productDispatch({
+            type: "CLEAR_FILTERS",
+          })
+        }
+      >
+        Remove Filters
+      </Button>
+    </div>
+  );
+};
+
+export default Filters;
